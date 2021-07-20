433844c1ffd53eeb5c51172fcdab7c1554dc2168
anunay_kumar <anunay_kumar@hotmail.com>
2021-07-20 13:50:27 +0530

    Created a react-boiler-plate and added firebase-config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDh6XEhVLeT8eQyfI7BRlIJrCcKCDAYFwQ",
+    authDomain: "challenge-1db10.firebaseapp.com",
+    projectId: "challenge-1db10",
+    storageBucket: "challenge-1db10.appspot.com",
+    messagingSenderId: "830197794085",
+    appId: "1:830197794085:web:a230a065530bc7113206cc",
+    measurementId: "G-2TH9CE8B7Y"
+  };
